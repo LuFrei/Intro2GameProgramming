4305db2dae5f7c01ea1030b1bf81b20c722ecdd3
Lucas F <lucasfreitas09876@gmail.com>
2017-10-18 15:45:53 -0400

    Enemy Setup
    made basic setup for enemy:
    -basic sprite, created empty script and such
    -made player hitbox smaller

---
+++ MidTerm/Assets/_Scripts/Enemy1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+
+
+    public bool isAttacking;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb2d.velocity = new Vector2(-speed, 0);
+	}
+}
